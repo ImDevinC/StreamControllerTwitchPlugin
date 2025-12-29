3774915d2fe3a83680dcafb7ea00fd0f2a27cef3
Devin Collins <3997333+ImDevinC@users.noreply.github.com>
2025-12-29 08:47:54 -0800

    feat(constants): add configuration constants file
    Create centralized constants.py for magic numbers

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""
+Constants used throughout the Twitch plugin.
+"""
+
+# Update intervals (in seconds)
+VIEWER_UPDATE_INTERVAL_SECONDS = 10
+AD_SCHEDULE_FETCH_INTERVAL_SECONDS = 30
+AD_DISPLAY_UPDATE_INTERVAL_SECONDS = 1
+CHAT_MODE_UPDATE_INTERVAL_SECONDS = 5
+
+# Error display
+ERROR_DISPLAY_DURATION_SECONDS = 3
+
+# OAuth/Authentication
+OAUTH_REDIRECT_URI = "http://localhost:3000/auth"
+OAUTH_PORT = 3000

ddc72f0e173f0931a6b5fffe63aed451418f5243
Devin Collins <3997333+ImDevinC@users.noreply.github.com>
2025-12-29 09:00:28 -0800

    feat(types): add type hints to settings and TwitchCore
    Add type hints to settings.py and TwitchCore

--- actions/TwitchCore.py
+++ actions/TwitchCore.py
@@ -2,6 +2,7 @@ from loguru import logger as log
 from src.backend.PluginManager.ActionCore import ActionCore
 from src.backend.DeckManagement.InputIdentifier import InputEvent, Input
 from src.backend.PluginManager.PluginSettings.Asset import Color, Icon
+from typing import Optional, Any
 
 from gi.repository import Gtk, Adw
 import gi
@@ -11,17 +12,17 @@ gi.require_version("Adw", "1")
 
 
 class TwitchCore(ActionCore):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
         # Setup AssetManager values
-        self.icon_keys = []
-        self.color_keys = []
-        self.current_icon: Icon = None
-        self.current_color: Color = None
-        self.icon_name = ""
-        self.color_name = ""
-        self.backend: 'Backend' = self.plugin_base.backend
+        self.icon_keys: list[str] = []
+        self.color_keys: list[str] = []
+        self.current_icon: Optional[Icon] = None
+        self.current_color: Optional[Color] = None
+        self.icon_name: str = ""
+        self.color_name: str = ""
+        self.backend: Any = self.plugin_base.backend
 
         self.plugin_base.asset_manager.icons.add_listener(self._icon_changed)
         self.plugin_base.asset_manager.colors.add_listener(self._color_changed)
@@ -30,9 +31,51 @@ class TwitchCore(ActionCore):
         self.create_generative_ui()
         self.create_event_assigners()
 
-    def on_ready(self):
+    def on_ready(self) -> None:
         super().on_ready()
         self.display_icon()
+        self.display_color()
+
+    def create_generative_ui(self) -> None:
+        pass
+
+    def create_event_assigners(self) -> None:
+        pass
+
+    def display_icon(self) -> None:
+        if not self.current_icon:
+            return
+        _, rendered = self.current_icon.get_values()
+        if rendered:
+            self.set_media(image=rendered)
+
+    async def _icon_changed(self, event: str, key: str, asset: Any) -> None:
+        if not key in self.icon_keys:
+            return
+        if key != self.icon_name:
+            return
+        self.current_icon = asset
+        self.icon_name = key
+        self.display_icon()
+
+    def display_color(self) -> None:
+        if not self.current_color:
+            return
+        color = self.current_color.get_values()
+        try:
+            self.set_background_color(color)
+        except:
+            # Sometimes we try to call this too early, and it leads to
+            # console errors, but no real errors. Ignoring this for now
+            pass
+
+    async def _color_changed(self, event: str, key: str, asset: Any) -> None:
+        if not key in self.color_keys:
+            return
+        if key != self.color_name:
+            return
+        self.current_color = asset
+        self.color_name = key
         self.display_color()
 
     def create_generative_ui(self):
--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from gi.repository import Gtk, Adw
 import gi
+from typing import Any
 
 from loguru import logger
 
@@ -18,20 +19,18 @@ class PluginSettings:
     _client_secret: Adw.PasswordEntryRow
     _auth_button: Gtk.Button
 
-    def __init__(self, plugin_base: PluginBase):
-        self._plugin_base = plugin_base
+    def __init__(self, plugin_base: PluginBase) -> None:
+        self._plugin_base: PluginBase = plugin_base
 
     def get_settings_area(self) -> Adw.PreferencesGroup:
         if not self._plugin_base.backend or not self._plugin_base.backend_initialized:
             self._status_label = Gtk.Label(
-                label=self._plugin_base.lm.get(
-                    "actions.base.credentials.failed"),
+                label=self._plugin_base.lm.get("actions.base.credentials.failed"),
                 css_classes=["twitch-controller-red"],
             )
         elif not self._plugin_base.backend.is_authed():
             self._status_label = Gtk.Label(
-                label=self._plugin_base.lm.get(
-                    "actions.base.credentials.failed"),
+                label=self._plugin_base.lm.get("actions.base.credentials.failed"),
                 css_classes=["twitch-controller-red"],
             )
         else:
@@ -53,8 +52,7 @@ class PluginSettings:
         self._auth_button.set_margin_top(10)
         self._auth_button.set_margin_bottom(10)
         self._client_id.connect("notify::text", self._on_change_client_id)
-        self._client_secret.connect(
-            "notify::text", self._on_change_client_secret)
+        self._client_secret.connect("notify::text", self._on_change_client_secret)
         self._auth_button.connect("clicked", self._on_auth_clicked)
 
         gh_link_label = self._plugin_base.lm.get("actions.info.link.label")
@@ -68,8 +66,7 @@ class PluginSettings:
         self._enable_auth()
 
         pref_group = Adw.PreferencesGroup()
-        pref_group.set_title(self._plugin_base.lm.get(
-            "actions.base.credentials.title"))
+        pref_group.set_title(self._plugin_base.lm.get("actions.base.credentials.title"))
         pref_group.add(self._status_label)
         pref_group.add(self._client_id)
         pref_group.add(self._client_secret)
@@ -77,34 +74,34 @@ class PluginSettings:
         pref_group.add(gh_label)
         return pref_group
 
-    def _load_settings(self):
+    def _load_settings(self) -> None:
         settings = self._plugin_base.get_settings()
         client_id = settings.get(KEY_CLIENT_ID, "")
         client_secret = settings.get(KEY_CLIENT_SECRET, "")
         self._client_id.set_text(client_id)
         self._client_secret.set_text(client_secret)
 
-    def _update_status(self, message: str, is_error: bool):
+    def _update_status(self, message: str, is_error: bool) -> None:
         style = "twitch-controller-red" if is_error else "twitch-controller-green"
         self._status_label.set_text(message)
         self._status_label.set_css_classes([style])
 
-    def _update_settings(self, key: str, value: str):
+    def _update_settings(self, key: str, value: str) -> None:
         settings = self._plugin_base.get_settings()
         settings[key] = value
         self._plugin_base.set_settings(settings)
 
-    def _on_change_client_id(self, entry, _):
+    def _on_change_client_id(self, entry: Any, _: Any) -> None:
         val = entry.get_text().strip()
         self._update_settings(KEY_CLIENT_ID, val)
         self._enable_auth()
 
-    def _on_change_client_secret(self, entry, _):
+    def _on_change_client_secret(self, entry: Any, _: Any) -> None:
         val = entry.get_text().strip()
         self._update_settings(KEY_CLIENT_SECRET, val)
         self._enable_auth()
 
-    def _on_auth_clicked(self, _):
+    def _on_auth_clicked(self, _: Any) -> None:
         if not self._plugin_base.backend:
             self._update_status("Failed to load backend", True)
             return
@@ -112,20 +109,17 @@ class PluginSettings:
         client_id = settings.get(KEY_CLIENT_ID)
         client_secret = settings.get(KEY_CLIENT_SECRET)
         self._plugin_base.auth_callback_fn = self._on_auth_completed
-        self._plugin_base.backend.update_client_credentials(
-            client_id, client_secret)
+        self._plugin_base.backend.update_client_credentials(client_id, client_secret)
 
-    def _enable_auth(self):
+    def _enable_auth(self) -> None:
         settings = self._plugin_base.get_settings()
         client_secret = settings.get(KEY_CLIENT_SECRET, "")
         client_id = settings.get(KEY_CLIENT_ID, "")
-        self._auth_button.set_sensitive(
-            len(client_id) > 0 and len(client_secret) > 0)
+        self._auth_button.set_sensitive(len(client_id) > 0 and len(client_secret) > 0)
 
-    def _on_auth_completed(self, success: bool, message: str = ""):
+    def _on_auth_completed(self, success: bool, message: str = "") -> None:
         self._enable_auth()
         if not message:
             lm_key = "authenticated" if success else "failed"
-            message = self._plugin_base.lm.get(
-                f"actions.base.credentials.{lm_key}")
+            message = self._plugin_base.lm.get(f"actions.base.credentials.{lm_key}")
         self._update_status(message, not success)
